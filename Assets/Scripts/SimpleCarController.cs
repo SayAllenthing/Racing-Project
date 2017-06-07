b38331abbdc784d170ad912f49cc6d7ea5a22319
SayAllenthing <allenofadams@hotmail.com>
2017-06-06 22:14:15 -0400

    Creating Repo

---
+++ Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SimpleCarController : MonoBehaviour 
+{
+	public List<AxleInfo> axleInfos; // the information about each individual axle
+	public float maxMotorTorque; // maximum torque the motor can apply to wheel
+	public float maxSteeringAngle; // maximum steer angle the wheel can have
+
+	// finds the corresponding visual wheel
+	// correctly applies the transform
+	public void ApplyLocalPositionToVisuals(WheelCollider collider, Vector3 offset)
+	{
+		if (collider.transform.childCount == 0) {
+			return;
+		}
+
+		Transform visualWheel = collider.transform.GetChild(0);
+
+		Vector3 position;
+		Quaternion rotation;
+		collider.GetWorldPose(out position, out rotation);
+
+		visualWheel.transform.localPosition = offset;
+		visualWheel.transform.rotation = rotation;
+	}
+
+	public void FixedUpdate()
+	{
+		float motor = maxMotorTorque * Input.GetAxis("Vertical");
+		float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+		foreach (AxleInfo axleInfo in axleInfos) 
+		{
+			if (axleInfo.steering) 
+			{
+				axleInfo.leftWheel.steerAngle = steering;
+				axleInfo.rightWheel.steerAngle = steering;
+			}
+			if (axleInfo.motor) 
+			{
+				axleInfo.leftWheel.motorTorque = motor;
+				axleInfo.rightWheel.motorTorque = motor;
+			}
+
+			ApplyLocalPositionToVisuals(axleInfo.leftWheel, axleInfo.Offset);
+			ApplyLocalPositionToVisuals(axleInfo.rightWheel, axleInfo.Offset);
+		}
+	}
+}
+
+[System.Serializable]
+public class AxleInfo 
+{
+	public WheelCollider leftWheel;
+	public WheelCollider rightWheel;
+	public bool motor; // is this wheel attached to motor?
+	public bool steering; // does this wheel apply steer angle?
+	public Vector3 Offset;
+}
